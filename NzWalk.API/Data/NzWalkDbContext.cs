516687bedd06d50e249e082a4cc51cc5075afd9a
Abhishek <codeMagnet@outlook.com>
2024-07-13 16:25:38 +0100

    Database and Enitity Framework added

---
+++ NzWalk.API/Data/NzWalkDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NzWalk.API.Models.Domain;
+
+namespace NzWalk.API.Data
+{
+    public class NzWalkDbContext : DbContext
+    {
+        public NzWalkDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        //Mapping with the Domain Models
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
--- NzWalk.API/Program.cs
+++ NzWalk.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NzWalk.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DI for connection string
+builder.Services.AddDbContext<NzWalkDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
 
 var app = builder.Build();
 
