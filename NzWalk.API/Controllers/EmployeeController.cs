63bd4a0d8a83fd44d8f37658797f0e6609e574c7
Abhishek <codeMagnet@outlook.com>
2024-07-13 15:38:30 +0100

    Add project files.

---
+++ NzWalk.API/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NzWalk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        string[] _employees = {"Abhishek", "Mansi", "Samaira"};
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            return Ok(_employees);
+        }
+
+        [HttpPost]
+        public IActionResult PostEmployees()
+        {
+            return Ok("This is post test");
+        }
+
+    }
+}
