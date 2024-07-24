683ddaf977d3616e1c1a27874212606b615df9aa
Abhishek <codeMagnet@outlook.com>
2024-07-24 20:06:39 +0100

    DTO Added

---
+++ NzWalk.API/Models/DTO/RegionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace NzWalk.API.Models.DTO
+{
+    public class RegionDto
+    {
+        // Basically what ever you want to expose from Domain Regions model we paste it here
+        // This looks repeat code but this is best way to do it.
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImageUrl { get; set; } //This can be null so we have put ?
+    }
+}
